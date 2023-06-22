0536f22b45b91d78842c52167c910b260fb67207
alimprasetyo77 <palim695@gmail.com>
2023-06-22 11:18:48 +0700

    fitur baru

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import ProductCart from "../components/Cart/ProductCart"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
@@ -8,23 +8,6 @@ import { useDispatch } from "react-redux"
 import { setActivePage } from "../features/NavigationSlice"
 
 const Cart = () => {
-<<<<<<< HEAD
-    useEffect(() => {
-        handlePageChange()
-      
-      }, [])
-      const dispatch = useDispatch()
-      const handlePageChange = () => {
-          dispatch(setActivePage('cart'))
-      }
-    return (
-        <>
-            <Header />
-            <ProductCart />
-            <Footer />
-        </>
-    )
-=======
   useEffect(() => {
     handlePageChange()
 
@@ -40,7 +23,6 @@ const Cart = () => {
       <Footer />
     </div>
   )
->>>>>>> master
 }
 
 export default Cart
